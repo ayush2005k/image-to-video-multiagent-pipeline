482f90087550061af423bb43f7c0f8e2a6ca8d5f
Ayush Singh <ayushsingh100k@gmail.com>
2026-07-05 00:02:14 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("🚀 Image-to-Video Multi-Agent Pipeline")
-print("Chapter 1 setup completed successfully!")
+from config import settings
+from utils.logger import console
+
+def main():
+
+    console.print("[green]Image-to-Video Multi-Agent Pipeline[/green]")
+
+    console.print(f"Retry Limit : {settings.RETRY_LIMIT}")
+
+    console.print(f"Assets Folder : {settings.IMAGE_FOLDER}")
+
+if __name__=="__main__":
+    main()

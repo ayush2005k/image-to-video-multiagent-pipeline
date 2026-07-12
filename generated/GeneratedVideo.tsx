d7408584a1c92610fffb2a24fcf9987dc6908d32
Ayush Singh <ayushsingh100k@gmail.com>
2026-07-12 19:44:10 +0530

    make changes genvideo.tsx file

--- generated/GeneratedVideo.tsx
+++ generated/GeneratedVideo.tsx
@@ -3,114 +3,102 @@ import {
     AbsoluteFill,
     Sequence,
     Img,
+    staticFile,
+    // useCurrentFrame,
+    // interpolate
 } from "remotion";
 
 export const GeneratedVideo: React.FC = () => {
     return (
-        <AbsoluteFill style={{ backgroundColor: "black" }}>
-            {/* Scene 1: UNLEASH THE ROAR! */}
+        <AbsoluteFill style={{
+            backgroundColor: "black"
+        }}>
+            {/* Scene 1: TRIUMPHANT ROAR! */}
             <Sequence from={0} durationInFrames={60}>
-                <Img src="JNIv57pSSv0AFHrK53iXE2EYPChKEG2rog3iIdJ7pswbfzHYBgrvZ0RicCQwWKf7onphtSo78volJ3FyGupoq7NzhjTlRD4EVAbrAgdxjLiR5DhzX8rNTqrKbwJJcCcmvHd8ByU.jpg" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                <AbsoluteFill style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                <Img src={staticFile("goal1.jpg")}
+                 style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover"
+                }} />
+                <div style={{
+                    position: "absolute",
+                    bottom: 20,
+                    width: "100%",
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    textShadow: "2px 2px 4px rgba(0,0,0,0.7)"
                 }}>
-                    <h1 style={{
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "5em",
-                        fontWeight: "bold",
-                        color: "white",
-                        textAlign: "center",
-                        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-                    }}>
-                        UNLEASH THE ROAR!
-                    </h1>
-                </AbsoluteFill>
+                    TRIUMPHANT ROAR!
+                </div>
             </Sequence>
 
-            {/* Scene 2: SOARING HIGH! */}
+            {/* Scene 2: VICTORY'S SWEET CONTRAST! */}
             <Sequence from={60} durationInFrames={60}>
-                <Img src="X4V1Gv7lT55cPxmQgV43zItWntVGeG99q6Mnf1jsBp19uuE7F15oBcWp5wqY895IOrOZ5U7DiU5bH0eFk1vY4maI6PuSms-ne7CHjo1tLRuOF740Feb1MOtyCUlvIAQAE9QiDpv.jpg" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                <AbsoluteFill style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                <Img src={staticFile("goal2.jpg")} 
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover"
+                }} />
+                <div style={{
+                    position: "absolute",
+                    bottom: 20,
+                    width: "100%",
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    textShadow: "2px 2px 4px rgba(0,0,0,0.7)"
                 }}>
-                    <h1 style={{
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "5em",
-                        fontWeight: "bold",
-                        color: "white",
-                        textAlign: "center",
-                        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-                    }}>
-                        SOARING HIGH!
-                    </h1>
-                </AbsoluteFill>
+                    VICTORY'S SWEET CONTRAST!
+                </div>
             </Sequence>
 
-            {/* Scene 3: PURE PASSION! */}
+            {/* Scene 3: UNBRIDLED JOY! */}
             <Sequence from={120} durationInFrames={60}>
-                <Img src="0sVwerLZ10GI2NUVz17BzbIpmGPn2tvrQYVe8E0FYBByN1DUtDoFQNReVLPXbQqO1P9h_OBh4joJkd4zmcT3cITDdz_nqbzhnQUHa3ClJhZs-8PMIkC3iEtJOKPESKVdPGwH3bi.jpg" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                <AbsoluteFill style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                <Img src={staticFile("goal3.jpg")}
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover"
+                }} />
+                <div style={{
+                    position: "absolute",
+                    bottom: 20,
+                    width: "100%",
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    textShadow: "2px 2px 4px rgba(0,0,0,0.7)"
                 }}>
-                    <h1 style={{
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "5em",
-                        fontWeight: "bold",
-                        color: "white",
-                        textAlign: "center",
-                        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-                    }}>
-                        PURE PASSION!
-                    </h1>
-                </AbsoluteFill>
+                    UNBRIDLED JOY!
+                </div>
             </Sequence>
 
-            {/* Scene 4: VICTORY SECURED! */}
+            {/* Scene 4: CROWD ERUPTS! */}
             <Sequence from={180} durationInFrames={60}>
-                <Img src="H8v7cpg5QtbdYVEtNV_tSgPEBip_g8wstUQxUdEf1gP_KkQJ8IHRU9_-xNQW6byKFc7pZJNRifKk0Cn7TL5hqwRzGnD-q-YBWWy9DyHjhAyf4EypRrKz1wJEweKUrIxbVe-.jpg" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                <AbsoluteFill style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                <Img src={staticFile("goal4.jpg")}
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover"
+                }} />
+                <div style={{
+                    position: "absolute",
+                    bottom: 20,
+                    width: "100%",
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    textShadow: "2px 2px 4px rgba(0,0,0,0.7)"
                 }}>
-                    <h1 style={{
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "5em",
-                        fontWeight: "bold",
-                        color: "white",
-                        textAlign: "center",
-                        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-                    }}>
-                        VICTORY SECURED!
-                    </h1>
-                </AbsoluteFill>
-            </Sequence>
-
-            {/* Scene 5: CHAMPIONS RISE! */}
-            <Sequence from={240} durationInFrames={60}>
-                <Img src="-b7E9cSSVXWprci6q_WWmg_2tB5tTQgCHdF60j7yHoQgKOY_UWQfAK7EGu8NJm4EkN7LjOhThjD8fShsOWoSCIsURLSsB-cHczFyGWIcrcnx6YfmyrmC8Ws2Zyk2DzfbhNSob8m.jpg" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                <AbsoluteFill style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(0, 0, 0, 0.4)",
-                }}>
-                    <h1 style={{
-                        fontFamily: "Arial, sans-serif",
-                        fontSize: "5em",
-                        fontWeight: "bold",
-                        color: "white",
-                        textAlign: "center",
-                        textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-                    }}>
-                        CHAMPIONS RISE!
-                    </h1>
-                </AbsoluteFill>
+                    CROWD ERUPTS!
+                </div>
             </Sequence>
         </AbsoluteFill>
     );
